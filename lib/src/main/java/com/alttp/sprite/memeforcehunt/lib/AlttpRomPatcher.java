73ddf82e7f9f8df42548ffe7f09ddfb2c80ea5e7
Benjamin Marwell <bmarwell@gmail.com>
2020-04-12 08:32:19 +0200

    [#6] patch cleanup

--- lib/src/main/java/com/alttp/sprite/memeforcehunt/lib/AlttpRomPatcher.java
+++ lib/src/main/java/com/alttp/sprite/memeforcehunt/lib/AlttpRomPatcher.java
@@ -17,36 +17,47 @@ public class AlttpRomPatcher {
      *
      * Shoutouts to Zarby89
      */
-    public static void patchROM(String romTarget, Skin s) throws IOException {
+    public static void patchROM(String romTarget, Skin skin) throws IOException {
+        byte[] romStream = readRom(romTarget);
+
+        writeSkin(romStream, skin);
+
+        writeRom(romTarget, romStream);
+    }
+
+    private static void writeRom(String romTarget, byte[] romStream) throws IOException {
+        try (FileOutputStream fsOut = new FileOutputStream(romTarget)) {
+            fsOut.write(romStream, 0, romStream.length);
+        }
+    }
+
+    private static void writeSkin(byte[] romStream, Skin skin) {
+        byte[] data = skin.getData();
+
+        // clear up space (safety)
+        int pos = OFFSET;
+        for (int i = 0; i < 950; i++, pos++) {
+            romStream[pos] = 0;
+        }
+
+        // write graphics
+        pos = OFFSET;
+        for (byte b : data) {
+            romStream[pos++] = b;
+        }
+
+        romStream[PAL_LOC] = skin.getPalette();
+        romStream[PAL_OW] = skin.getPaletteOW();
+    }
+
+    private static byte[] readRom(String romTarget) throws IOException {
         byte[] romStream;
 
         try (FileInputStream fsInput = new FileInputStream(romTarget)) {
             romStream = new byte[(int) fsInput.getChannel().size()];
             fsInput.read(romStream);
             fsInput.getChannel().position(0);
-            fsInput.close();
-
-            try (FileOutputStream fsOut = new FileOutputStream(romTarget)) {
-                byte[] data = s.getData();
-
-                // clear up space (safety)
-                int pos = OFFSET;
-                for (int i = 0; i < 950; i++, pos++) {
-                    romStream[pos] = 0;
-                }
-
-                // write graphics
-                pos = OFFSET;
-                for (byte b : data) {
-                    romStream[pos++] = b;
-                }
-
-                romStream[PAL_LOC] = s.getPalette();
-                romStream[PAL_OW] = s.getPaletteOW();
-
-                fsOut.write(romStream, 0, romStream.length);
-                fsOut.close();
-            }
         }
+        return romStream;
     }
 }
